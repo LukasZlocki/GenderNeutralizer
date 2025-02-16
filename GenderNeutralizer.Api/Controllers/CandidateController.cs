5139129d209448cacd53e2539d25c3c5dd54a0c9
lzlocki <lzlocki@poczta.fm>
2025-02-16 20:41:02 +0100

    add controller with getallcandidates

---
+++ GenderNeutralizer.Api/Controllers/CandidateController.cs
@@ -0,0 +1,24 @@
+﻿using GenderNeutralizer.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenderNeutralizer.Api.Controllers
+{
+    public class CandidateController : Controller
+    {
+        private readonly CandidateService _dbCandidate;
+
+        public CandidateController(CandidateService dbCandidate)
+        {
+            _dbCandidate = dbCandidate;
+        }
+
+        // GET
+        [HttpGet("api/candidate")]
+        public IActionResult GetAllCandidates()
+        {
+            var candidates = _dbCandidate.GetAllCandidates();
+            return Ok(candidates);
+        }
+
+    }
+}
