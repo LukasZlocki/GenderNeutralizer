0901af865c434946d7758f3cb585594aaa805981
lzlocki <lzlocki@poczta.fm>
2025-02-16 22:09:30 +0100

    add serializer

---
+++ GenderNeutralizer.Api/Serializer/SerializeCandidateReadDTO.cs
@@ -0,0 +1,20 @@
+﻿using GenderNeutralizer.Api.ModelDtos;
+using GenderNeutralizer.Api.Models;
+
+namespace GenderNeutralizer.Api.DTOs
+{
+    public static class SerializeCandidateReadDTO
+    {
+        public static Candidate SerializeReadDtoToCandidateObject(CandidateReadDto readDto)
+        {
+            return new Candidate
+            {
+                CandidateName = readDto.CandidateName,
+                CandidateLastName = readDto.CandidateLastName,
+                TextCv = readDto.TextCv,
+                TextCvNeutralized = readDto.TextCvNeutralized
+            };
+        }
+
+    }
+}
