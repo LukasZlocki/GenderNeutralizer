11af45c6dffaa0b5ea0342a0e0162eb61b16e75a
lzlocki <lzlocki@poczta.fm>
2025-02-16 19:03:19 +0100

    setup database and perform first migration

---
+++ GenderNeutralizer.Api/Migrations/20250216175543_initMigration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GenderNeutralizer.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class initMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Candidates",
+                columns: table => new
+                {
+                    CandidateId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CandidateName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CandidateLastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TextCv = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TextCvNeutralized = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidates", x => x.CandidateId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Candidates");
+        }
+    }
+}
