ad8f8b3732b534cd0f424ec542e35cd15f8febf7
lzlocki <lzlocki@poczta.fm>
2025-02-16 22:09:06 +0100

    add data transfer model

---
+++ GenderNeutralizer.Api/DTOs/CandidateReadDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GenderNeutralizer.Api.ModelDtos
+{
+    public class CandidateReadDto
+    {
+        public string CandidateName { get; set; }
+        public string CandidateLastName { get; set; }
+        public string TextCv { get; set; }
+        public string TextCvNeutralized { get; set; }
+    }
+}
