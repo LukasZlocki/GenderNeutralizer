698e11aac40c008199c30815c0e7588e63378e75
lzlocki <lzlocki@poczta.fm>
2025-02-16 18:23:59 +0100

    add candidate model

---
+++ GenderNeutralizer.Api/Models/Candidate.cs
@@ -0,0 +1,11 @@
+﻿namespace GenderNeutralizer.Api.Models
+{
+    public class Candidate
+    {
+        public Guid CandidateId { get; set; }
+        public string CandidateName { get; set; }
+        public string CandidateLastName{ get; set; }
+        public string TextCv { get; set; }
+        public string TextCvNeutralized { get; set; }
+    }
+}
