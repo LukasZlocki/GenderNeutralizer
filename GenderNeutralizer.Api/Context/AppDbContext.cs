cac963c54584396de2d27f3f48cc85bb8a4c7cd0
lzlocki <lzlocki@poczta.fm>
2025-02-16 19:02:14 +0100

    untrack files. add db context

---
+++ GenderNeutralizer.Api/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using GenderNeutralizer.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace GenderNeutralizer.Api.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Candidate> Candidates { get; set; }
+    }
+}
