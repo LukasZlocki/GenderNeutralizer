29466967ff5eb5a84060632b0ef0057b43aecbc5
lzlocki <lzlocki@poczta.fm>
2025-05-14 19:10:30 +0200

    add candidate model

---
+++ GenderNeutralizer.App/GenderNeutralizer.App/Models/Candidate.cs
@@ -0,0 +1,11 @@
+﻿namespace GenderNeutralizer.App.Models
+{
+    public class Candidate
+    {
+        public int CandidateId { get; set; }
+        public string? CandidateName { get; set; }
+        public string? CandidateLastName { get; set; }
+        public string? TextCv { get; set; }
+        public string? TextCvNeutralized { get; set; }
+    }
+}
