4801269c9bc5a8f8236fbe7d1827cb966f27cb60
lzlocki <lzlocki@poczta.fm>
2025-05-14 19:39:48 +0200

    add service to write file to disc

---
+++ GenderNeutralizer.App/GenderNeutralizer.App/Services/ILocalDriveService.cs
@@ -0,0 +1,7 @@
+﻿namespace GenderNeutralizer.App.Services
+{
+    public interface ILocalDriveService
+    {
+        public Task<bool> UploadCV(IFormFile file);
+    }
+}
---
+++ GenderNeutralizer.App/GenderNeutralizer.App/Services/LocalDriveService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GenderNeutralizer.App.Services
+{
+    public class LocalDriveService : ILocalDriveService
+    {
+        public async Task<bool> UploadCV(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return false;
+
+            var targetDirectory = @"C:\VirtualServer\GenderNeutralizer";
+
+            if (!Directory.Exists(targetDirectory))
+                Directory.CreateDirectory(targetDirectory);
+
+            var filePath = Path.Combine(targetDirectory, file.FileName);
+
+            using var stream = new FileStream(filePath, FileMode.Create);
+            await file.CopyToAsync(stream);
+
+            return true;
+        }
+    }
+}
